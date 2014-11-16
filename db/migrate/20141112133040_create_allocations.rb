ac3fb9cadc233dc9df60bc6a6c00a8be3ced9b31
David Fisher <tibbon@gmail.com>
2014-11-16 13:12:09 -0500

    Added a not null constraint to both foreign keys in allcations table

--- db/migrate/20141112133040_create_allocations.rb
+++ db/migrate/20141112133040_create_allocations.rb
@@ -1,8 +1,8 @@
 class CreateAllocations < ActiveRecord::Migration
   def change
     create_table :allocations do |t|
-      t.belongs_to :user, index: true
-      t.belongs_to :staffing_request, index: true
+      t.belongs_to :user, index: true, null: false
+      t.belongs_to :staffing_request, index: true, null: false
 
       t.timestamps null: false
     end
