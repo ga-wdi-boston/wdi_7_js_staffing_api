56fc141aafcb22b4cd0259e2dcb37883ee9ce6af
Dan Johnson <dan@balancerockmedia.com>
2014-11-21 09:32:18 -0500

    edits

--- db/migrate/20141112145150_create_skills_staffing_requests.rb
+++ db/migrate/20141112145150_create_skills_staffing_requests.rb
@@ -1,7 +1,7 @@
 class CreateSkillsStaffingRequests < ActiveRecord::Migration
   def change
     create_table :skills_staffing_requests do |t|
-    	t.belongs_to :skill, null: false
+      t.belongs_to :skill, null: false
       t.belongs_to :staffing_request, null: false
     end
   end
