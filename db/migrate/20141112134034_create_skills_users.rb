601e7d17a5043bd94855aa19894824d7f945e513
Dan Johnson <dan@balancerockmedia.com>
2014-11-21 09:29:57 -0500

    edits

--- db/migrate/20141112134034_create_skills_users.rb
+++ db/migrate/20141112134034_create_skills_users.rb
@@ -1,8 +1,8 @@
 class CreateSkillsUsers < ActiveRecord::Migration
   def change
     create_table :skills_users do |t|
-    	t.belongs_to :skill
-      t.belongs_to :user
+			t.belongs_to :skill
+			t.belongs_to :user
     end
   end
 end
